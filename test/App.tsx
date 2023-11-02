a96055b47514bb90555b5ab2a9b7fe2c151ea05d
Ishat Gupta <62379502+LePichu@users.noreply.github.com>
2023-11-02 23:59:21 +0530

    feat: bundling test

--- test/App.tsx
+++ test/App.tsx
@@ -1,3 +1,7 @@
 // deno-lint-ignore-file
 const nya = <h1>Meow.</h1>
 const typetest: any = (<></>)
+
+function App() {
+	return <h1>Meow~!</h1>
+}
--- test/build.ts
+++ test/build.ts
@@ -1,10 +1,12 @@
 import { build } from "https://deno.land/x/esbuild@v0.19.2/mod.js"
+import { httpImports } from "https://deno.land/x/esbuild_plugin_http_imports@v1.3.0/index.ts"
 import { SolidPlugin } from "../mod.ts"
 
 const res = await build({
 	entryPoints: [
 		"./test/App.tsx",
 	],
+	bundle: Boolean(Deno.args[1]),
 	format: "esm",
 	target: "esnext",
 	jsx: "preserve",
@@ -14,11 +16,13 @@ const res = await build({
 			generate: Deno.args[0] as any,
 			hydrateable: Boolean(Deno.args[1])
 		}),
+		// @ts-ignore "Hope it works"
+		httpImports()
 	],
 	write: false
 })
 
-for (const out of res.outputFiles) {
+for (const out of res.outputFiles!) {
 	console.log(out.text)
 }
 
